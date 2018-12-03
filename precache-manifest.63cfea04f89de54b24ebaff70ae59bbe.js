bf39824c2efc907b96ce173e9e748a2a18cf4a2d
rowshenpaltayev <rowshenpaltayev@gmail.com>
2018-12-02 19:46:13 -0600

    Updates

---
+++ precache-manifest.63cfea04f89de54b24ebaff70ae59bbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "54ba80e479460e64e3c1",
+    "url": "/RoboFriends/static/js/main.54ba80e4.chunk.js"
+  },
+  {
+    "revision": "77597e1daefac5d8fc3c",
+    "url": "/RoboFriends/static/js/1.77597e1d.chunk.js"
+  },
+  {
+    "revision": "54ba80e479460e64e3c1",
+    "url": "/RoboFriends/static/css/main.573c57e6.chunk.css"
+  },
+  {
+    "revision": "77597e1daefac5d8fc3c",
+    "url": "/RoboFriends/static/css/1.68637dd2.chunk.css"
+  },
+  {
+    "revision": "b56950adcd5941750e9a9e715fe87423",
+    "url": "/RoboFriends/index.html"
+  }
+];
